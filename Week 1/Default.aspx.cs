67c98496495449a747ba8ea4bd258641517cc611
WaynellLovell <wlovell2@my.centennialcollege.ca>
2016-09-07 21:39:21 -0400

    added code

--- Week 1/Default.aspx.cs
+++ Week 1/Default.aspx.cs
@@ -9,9 +9,14 @@ namespace Week_1
 {
     public partial class _Default : Page
     {
+        /// <summary>
+        /// This method executes everytime the page loads
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            HelloLable.Text = "Good Bye.";
         }
     }
 }
--- Week 1/Default.aspx.designer.cs
+++ Week 1/Default.aspx.designer.cs
@@ -13,12 +13,12 @@ namespace Week_1 {
     public partial class _Default {
         
         /// <summary>
-        /// Label1 control.
+        /// HelloLable control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
+        protected global::System.Web.UI.WebControls.Label HelloLable;
     }
 }
